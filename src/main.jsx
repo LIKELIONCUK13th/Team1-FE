72bcd51040b707490743a9532aa2a8ecf3a319db
yousung <doyousung@naver.com>
2025-07-14 22:10:44 +0900

    [main] Basecode Set

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+// main.jsx -> 엔트리포인트, App.jsx -> 랜더링 집합
